006ff3de8c49ec69ddfe6ec2822a8e4827087f42
Karina Koreniuk <karikoreniuk@gmail.com>
2024-06-05 21:11:47 +0300

    dom 4 first commit

---
+++ kalkul.py
@@ -0,0 +1,14 @@
+a = int(input("Введите число a: "))
+b = int(input("Введите число b: "))
+sum = a + b
+dif = a - b
+multiplication = a * b
+division = a / b
+module_division = a % b
+exponentiation = a ** b
+# print ("Сумма чисел равна: ", sum)
+# print ("Разница чисел равна: ", dif)
+# print ("Произведение чисел равно: ", multiplication)
+# print ("Частное от деления чисел равно: ", division)
+# print ("Остаток от деления чисел равен: ", module_division)
+# print ("Результат возведения числа в степень равен: ", exponentiation)
---
+++ text.py
@@ -0,0 +1,6 @@
+text = input()
+print(text.upper())
+text_a = text.replace("а", "")
+print(text_a)
+text_end = text.split()
+print(text_end[-1])
